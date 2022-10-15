ec3b8248734270a1200ad22cdf970a841f322ccb
Florent Monin <florent@digitaledge.se>
2022-10-15 22:51:51 +0200

    ADD main name to index page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,13 @@
 <script>
 
+    import MainName from "../components/MainName.svelte";
+	let name = "Florent Monin"
 </script>
 
 <svelte:head>
+	<title>{name}</title>
 </svelte:head>
+
+<div class="flex lg:flex-row flex-col lg:place-content-center bg-sky">
+	<MainName/>
+</div>
