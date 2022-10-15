98d29d539a92fbd65927a2dfd64b3398454e8a44
Florent Monin <florent@digitaledge.se>
2022-10-15 23:09:53 +0200

    ADD menu with links

---
+++ src/components/Menu.svelte
@@ -0,0 +1,8 @@
+<script>
+    export let segment;
+</script>
+
+
+<div class="hover:border-2 rounded-lg hover:bg-white p-1 px-4 hover:text-stone-dark"><a aria-current="{segment === undefined ? 'page' : undefined}" href=".">About Me</a></div>
+<div class="hover:border-2 rounded-lg hover:bg-white p-1 px-4 hover:text-stone-dark"><a aria-current="{segment === 'projects' ? 'page' : undefined}" href="projects">Projects</a></div>
+<div class="hover:border-2 rounded-lg hover:bg-white p-1 px-4 hover:text-stone-dark"><a aria-current="{segment === 'contact' ? 'page' : undefined}" href="contact">Contact</a></div>
