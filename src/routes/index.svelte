68f102f78aa3221cdd951c15303427050be85206
Florent Monin <florent@digitaledge.se>
2022-10-16 18:49:18 +0200

    ADD new block index

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -13,3 +13,22 @@
 	<MainName/>
 	<Whoami/>
 </div>
+
+
+<div class="flex relative bg-sky h-screen">
+	<!-- <div
+		class="bg-stone z-10 left-0 opacity-50 absolute screen-height lg:w-1/2 2xl:ml-24 lg:ml-12 w-full"></div>
+	<div
+		class="bg-stone z-20 left-0 opacity-50 absolute screen-height lg:w-1/2 2xl:ml-16 lg:ml-8 w-11/12"></div>
+	<div
+		class="bg-stone z-30 left-0 opacity-50 absolute screen-height lg:w-1/2 2xl:ml-8 lg:ml-4 w-10/12"></div>
+	<div
+		class="bg-stone z-40 absolute left-0 lg:w-1/2 w-9/12 screen-height flex items-center justify-center text-white">
+		<div class="text-9xl text-cocoa font-bebas pl-20 py-10">
+			Skills
+		</div>
+	</div> -->
+	<div class="text-9xl text-cocoa font-bebas pl-20 py-10">
+			Skills
+	</div>
+</div>
